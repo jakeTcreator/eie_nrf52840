30132aac9db249b8d70d6a67fcae7f03943c07f7
jakeTcreator <jake@tanyi.ca>
2025-10-02 20:26:32 -0600

    Advanced light sequence

--- app/src/main.c
+++ app/src/main.c
@@ -2,12 +2,38 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
+#define LED3_NODE DT_ALIAS(led3)
+
+
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
+
+static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 int main(void){
 	int ret;
-	
+	int ret1;
+	int ret2;
+	int ret3;	
+
 	if(!gpio_is_ready_dt(&led0)) {
+		return -1;
+	}
+
+	if(!gpio_is_ready_dt(&led1)) {
+		return -1;
+	}
+
+	if(!gpio_is_ready_dt(&led2)) {
+		return -1;
+	}
+
+	if(!gpio_is_ready_dt(&led3)) {
 		return -1;
 	}
 
@@ -16,8 +42,39 @@ int main(void){
 		return ret;
 	}
 
-	while (1) {
+	
 
+	ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret1 < 0) {
+		return ret1;
+	}
+
+
+	ret2 = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	if (ret2 < 0) {
+		return ret2;
+	}
+
+
+	ret3 = gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+	if (ret3 < 0) {
+		return ret3;
+	}
+	while (1) {
+		gpio_pin_set_dt(&led0, 1);			
+		k_msleep(1000);
+		gpio_pin_set_dt(&led0, 0);		
+		gpio_pin_set_dt(&led1, 1);
+
+		k_msleep(1000);
+		gpio_pin_set_dt(&led1, 0);
+		gpio_pin_set_dt(&led3, 1);
+
+		k_msleep(1000);
+		gpio_pin_set_dt(&led3, 0);
+		gpio_pin_set_dt(&led2, 1);
+		k_msleep(1000);
+		gpio_pin_set_dt(&led2, 0);
 	}
 
 	return 0;
